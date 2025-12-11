3662edc46a8a5ce58dfe11b243d709a2731bc0c4
Gonzalo Perez <gperez@vgl.cl>
2025-12-11 17:21:10 -0300

    log color

--- logger_setup.py
+++ logger_setup.py
@@ -1,20 +1,24 @@
 import logging
 from logging.handlers import RotatingFileHandler
+from colorlog import ColoredFormatter
 
 def setup_logger(name: str = "app", log_file: str = "app.log", level: int = logging.INFO, max_bytes: int = 5_000_000, backup_count: int = 3):
     logger = logging.getLogger(name)
     logger.setLevel(level)
     logger.propagate = False
 
-    formatter = logging.Formatter("[%(asctime)s] [%(levelname)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S" )
+    color_formatter = ColoredFormatter("%(log_color)s[%(asctime)s] [%(levelname)s] %(message)s", datefmt = "%Y-%m-%d %H:%M:%S", log_colors={"DEBUG": "cyan", "INFO": "green", "WARNING": "yellow", "ERROR": "red", "CRITICAL": "red, bold" })
+    #color_formatter = ColoredFormatter("%(log_color)s[%(asctime)s] [%(levelname)s]%(reset)s %(message)s", datefmt = "%Y-%m-%d %H:%M:%S", log_colors={"DEBUG": "cyan", "INFO": "green", "WARNING": "yellow", "ERROR": "red", "CRITICAL": "red, bold" })
+    
+    file_formatter = logging.Formatter("[%(asctime)s] [%(levelname)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S" )
 
     ch = logging.StreamHandler()
     ch.setLevel(level)
-    ch.setFormatter(formatter)
+    ch.setFormatter(color_formatter)
 
     fh = RotatingFileHandler(log_file, maxBytes = max_bytes, backupCount = backup_count)
     fh.setLevel(level)
-    fh.setFormatter(formatter)
+    fh.setFormatter(file_formatter)
 
     if not logger.handlers:
         logger.addHandler(ch)
--- main.py
+++ main.py
@@ -5,16 +5,19 @@ from logger_setup import setup_logger
 from requests.exceptions import Timeout
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
-
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+logger = setup_logger("cdn_report", "cdn_report.log")
 
 def check_dns_tcp(host, port=53, timeout=5):
     try:
         with socket.create_connection((host, port), timeout=timeout) as s:
-            print('Connection succesfully established to VPN')
+            logger.info('Connection succesfully established to VPN')
     except socket.timeout:
+        logger.error('Connection timed out, Please connect to VPN')
         raise TimeoutError(f"Connection timed out, Please connect to VPN")
     except Exception as e:
+        logger.error(f"DNS TCP check failed: {e}")
         raise Exception(f"DNS TCP check failed: {e}")
 
 print(check_dns_tcp('172.17.169.41'))
@@ -22,16 +25,17 @@ print(check_dns_tcp('172.17.169.41'))
 try:
     with open('hosts.json', 'r') as file:
         hosts = json.load(file)
+        logger.info('Hosts JSON file succesfully loaded')
 except FileNotFoundError:
-    print('File not found') 
+    logger.error('File not found') 
 except json.JSONDecodeError:
-    print('Invalid JSON Format')
+    logger.error('Invalid JSON Format')
 
 for host in hosts:
     url = f'https://{host["ip"]}/json/health_summary?'
     username = 'admin'
     password = 'cl4r0vtr'
-    print(f'\033[36mAnalyzing host: {host["hostname"]}, ilo ip:{host["ip"]}\033[0m')
+    logger.info(f'Analyzing host: {host["hostname"]}, ilo ip:{host["ip"]}')
     try:
         response = requests.get(url, auth=(username, password), verify=False, timeout=5).json()
         keys = ['self_test', 'system_health', 'hostpwr_state', 'fans_status', 'fans_redundancy', 'temperature_status', 'power_supplies_status', 'power_supplies_redundancy', 'power_supplies_mismatch', 'storage_status', 'nic_status', 'cpu_status', 'mem_status', 'ext_hlth_status']
@@ -39,8 +43,8 @@ for host in hosts:
         for key, value in response.items():
             if key in keys:
                 if value in values:
-                    print(f'\033[92m{key}: {value} --> PASS\033[0m')
+                    logger.info(f'{key}: {value} --> PASS')
                 else:
-                    print(f'\033[91m{key}: {value} --> FAILED\033[0m')
+                    logger.error(f'{key}: {value} --> FAILED')
     except Timeout:
-        print(f'\033[91mHost: {host["hostname"]}, ilo ip:{host["ip"]} request timed out\033[0m')
+        logger.error(f'Host: {host["hostname"]}, ilo ip:{host["ip"]} request timed out')
