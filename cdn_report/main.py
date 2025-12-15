bc666256bc51b68ceffa44a60a8824752f1cf0bb
Gonzalo Perez <gperez@vgl.cl>
2025-12-15 15:58:05 -0300

    Refactor: introduce structured project layout

---
+++ cdn_report/main.py
@@ -0,0 +1,50 @@
+import json
+import requests
+import socket
+from logger_setup import setup_logger
+from requests.exceptions import Timeout
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+logger = setup_logger("cdn_report", "cdn_report.log")
+
+def check_dns_tcp(host, port=53, timeout=5):
+    try:
+        with socket.create_connection((host, port), timeout=timeout) as s:
+            logger.info('Connection succesfully established to VPN')
+    except socket.timeout:
+        logger.error('Connection timed out, Please connect to VPN')
+        raise TimeoutError(f"Connection timed out, Please connect to VPN")
+    except Exception as e:
+        logger.error(f"DNS TCP check failed: {e}")
+        raise Exception(f"DNS TCP check failed: {e}")
+
+print(check_dns_tcp('172.17.169.41'))
+
+try:
+    with open('hosts.json', 'r') as file:
+        hosts = json.load(file)
+        logger.info('Hosts JSON file succesfully loaded')
+except FileNotFoundError:
+    logger.error('File not found') 
+except json.JSONDecodeError:
+    logger.error('Invalid JSON Format')
+
+for host in hosts:
+    url = f'https://{host["ip"]}/json/health_summary?'
+    username = 'admin'
+    password = 'cl4r0vtr'
+    logger.info(f'Analyzing host: {host["hostname"]}, ilo ip:{host["ip"]}')
+    try:
+        response = requests.get(url, auth=(username, password), verify=False, timeout=5).json()
+        keys = ['self_test', 'system_health', 'hostpwr_state', 'fans_status', 'fans_redundancy', 'temperature_status', 'power_supplies_status', 'power_supplies_redundancy', 'power_supplies_mismatch', 'storage_status', 'nic_status', 'cpu_status', 'mem_status', 'ext_hlth_status']
+        values = ['OP_STATUS_OK', 'ON', 'REDUNDANT', 0]
+        for key, value in response.items():
+            if key in keys:
+                if value in values:
+                    logger.info(f'{key}: {value} --> PASS')
+                else:
+                    logger.error(f'{key}: {value} --> FAILED')
+    except Timeout:
+        logger.error(f'Host: {host["hostname"]}, ilo ip:{host["ip"]} request timed out')
